87f4ab7ba28c92fb040cca79c476b235f50d074b
JoaoMichelS <jovitor931@gmail.com>
2024-03-13 12:05:38 -0300

    Update/Delete User

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -37,4 +37,43 @@ public class UserController : ControllerBase
 
         return Ok(newUser);
     }
+
+    [HttpPut("updateUser/{id}", Name = "updateUser")]
+    public async Task<IActionResult> UpdateUser(int id, [FromBody] User updatedUser)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var user = _context.User.FirstOrDefault(x => x.user_id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        user.name = updatedUser.name;
+        user.cpf = updatedUser.cpf;
+        user.phone = updatedUser.phone;
+        user.admin = updatedUser.admin;
+
+        await _context.SaveChangesAsync();
+
+        return Ok(user);
+    }
+
+    [HttpDelete("deleteUser/{id}", Name = "deleteUser")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        var user = _context.User.FirstOrDefault(x => x.user_id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _context.User.Remove(user);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
 }
